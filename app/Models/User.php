769ee08b6a77ab63198e1214f5b5fa0a4872c6c2
douglasrono <douglasronno@gmail.com>
2023-04-21 15:37:38 +0300

    modified user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
+    use HasApiTokens,  Notifiable,SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -19,7 +18,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-
         'email',
         'password',
     ];
