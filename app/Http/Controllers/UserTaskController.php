39aecf3ea0a5d1111231542d29e4bd25025facc2
douglasrono <douglasronno@gmail.com>
2023-04-21 15:32:14 +0300

    created user task logic

---
+++ app/Http/Controllers/UserTaskController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User_task;
+use Illuminate\Http\Request;
+
+class UserTaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User_task  $user_task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User_task $user_task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User_task  $user_task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User_task $user_task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User_task  $user_task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User_task $user_task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User_task  $user_task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User_task $user_task)
+    {
+        //
+    }
+}
